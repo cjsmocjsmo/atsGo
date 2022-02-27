c6c56ea3e4d433eab939496ed97ee2e4f54f822b
cjsmocjsmo <porthose.cjsmo.cjsmo@gmail.com>
2022-02-27 06:34:24 -0800

    convert pics to webp removed origs

--- static/gallery/portrait/webpconvert.sh
+++
@@ -1,7 +0,0 @@
-for f in /home/charliepi/go/atsGo/static/gallery/portrait/*.jpg; do
-cwebp -q 95 "$f" -o "${f%.jpg}.webp"
-done
-
-for f in /home/charliepi/go/atsGo/static/gallery/portrait/*.jpg; do
-cwebp -q 95 -resize 300 0 "$f" -o "${f%.jpg}_thumb.webp"
-done
